1f0226abcf5117885c0b618cd518f7c42ce6a69a
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2016-12-08 21:02:37 +0100

    Redefine the lens type and most basic operators.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 type Lens<A, B> = {
-    get: () => B;
-    set: (element: A) => B;
-    at: <K extends keyof B>(prop: K) => Lens<A, B[K]>;
+    get: (item: A) => B;
+    set: (item: A, part: B) => A;
 };
+
+function pure<A>(): Lens<A, A> {
+    return {
+        get: (item: A) => item,
+        set: (_item: A, part: A) => part
+    };
+}
+
+function compose<A, B, C>(bc: Lens<B, C>, ab: Lens<A, B>): Lens<A, C> {
+    return {
+        get: (element: A) => bc.get(ab.get(element)),
+        set: (item: A, part: C) => ab.set(item, bc.set(ab.get(item), part))
+    };
+}
