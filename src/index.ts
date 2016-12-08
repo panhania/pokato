c3b20105db5b8ac60bb99cb61427ac895d168f3d
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2016-12-08 20:36:51 +0100

    Declare the basic pseudo-lens type.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-function hello(name: string) {
-    console.log(`hello ${name}!`);
-}
-
-hello("world");
+type Lens<A, B> = {
+    get: () => B;
+    set: (element: A) => B;
+    at: <K extends keyof B>(prop: K) => Lens<A, B[K]>;
+};
