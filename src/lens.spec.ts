5b8b248fd48a5ea1bcc4bbba026d6758c610fa8a
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2016-12-09 11:42:00 +0100

    Improve grammar in test case messages.

--- src/lens.spec.ts
+++ src/lens.spec.ts
@@ -5,13 +5,13 @@ import { pure, view, compose } from "./lens";
 
 describe("pure", () => {
 
-    it("should have getter like the I combinator", () => {
+    it("should have a getter like the I combinator", () => {
         expect(pure().get(42)).equals(42);
         expect(pure().get("foo")).equals("foo");
         expect(pure().get({ foo: 42 })).to.deep.equal({ foo: 42 });
     });
 
-    it("should have setter like the K combinator", () => {
+    it("should have a setter like the K combinator", () => {
         expect(pure().set(-1, 42)).equals(42);
         expect(pure().set(42, -1)).equals(-1);
         expect(pure().set("foo", "bar")).equals("bar");
