343f4112143a9b46176768cba200f0a0dde1ddd5
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2016-12-10 07:18:44 +0100

    Rename pseudo-lens objects to focus objects.

--- src/pseudolens.ts
+++ src/pseudolens.ts
@@ -1,7 +1,7 @@
 import { Lens, pure, view, compose } from "./lens";
 
 
-export class PseudoLens<A, B> {
+export class Focus<A, B> {
 
     constructor(private item: A, private lens: Lens<A, B>) {
     }
@@ -18,15 +18,15 @@ export class PseudoLens<A, B> {
         return this.lens.set(this.item, f(this.lens.get(this.item)));
     }
 
-    focus<C>(lens: Lens<B, C>): PseudoLens<A, C> {
-        return new PseudoLens(this.item, compose(lens, this.lens));
+    focus<C>(lens: Lens<B, C>): Focus<A, C> {
+        return new Focus(this.item, compose(lens, this.lens));
     }
 
-    at<K extends keyof B>(prop: K): PseudoLens<A, B[K]> {
+    at<K extends keyof B>(prop: K): Focus<A, B[K]> {
         return this.focus<B[K]>(view<B, K>(prop));
     }
 }
 
-export function lens<A>(item: A): PseudoLens<A, A> {
-    return new PseudoLens(item, pure<A>());
+export function focus<A>(item: A): Focus<A, A> {
+    return new Focus(item, pure<A>());
 }
