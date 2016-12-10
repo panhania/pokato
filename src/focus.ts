60f6568b997d38353838c3b5098f9614cc7fe06a
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2016-12-10 17:38:52 +0100

    Fix linter warnings about implicit access modifiers.

--- src/focus.ts
+++ src/focus.ts
@@ -6,23 +6,23 @@ export class Focus<A, B> {
     constructor(private item: A, private lens: Lens<A, B>) {
     }
 
-    get(): B {
+    public get(): B {
         return this.lens.get(this.item);
     }
 
-    set(part: B): A {
+    public set(part: B): A {
         return this.lens.set(this.item, part);
     }
 
-    modify(f: (part: B) => B): A {
+    public modify(f: (part: B) => B): A {
         return this.lens.set(this.item, f(this.lens.get(this.item)));
     }
 
-    with<C>(lens: Lens<B, C>): Focus<A, C> {
+    public with<C>(lens: Lens<B, C>): Focus<A, C> {
         return new Focus(this.item, compose(lens, this.lens));
     }
 
-    at<K extends keyof B>(prop: K): Focus<A, B[K]> {
+    public at<K extends keyof B>(prop: K): Focus<A, B[K]> {
         return this.with<B[K]>(view<B, K>(prop));
     }
 }
